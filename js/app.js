eb99c0bf433da704b3ca7f34cde0bc7d8edf4438
obswork <obssabizuwork@gmail.com>
2016-07-03 11:27:07 -0400

    added route configs via ngRoute config and angular-route.js cdn-source to index

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-angular.module('app', ['7minWorkout']);
+angular.module('app', ['ngRoute', '7minWorkout']);
 angular.module('7minWorkout', []);
+config(function ($routeProvider) {
+    $routeProvider.when('/start', {
+        templateUrl: 'partials/start.html'
+    });
+    $routeProvider.when('/workout', {
+        templateUrl: 'partials/workout.html',
+        controller: 'WorkoutController'
+    });
+    $routeProvider.when('/finish', {
+        templateUrl: 'partials/finish.html'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/start'
+    });
+});
